ba56f4f2918ff35d7c6698b5e40fea492b9a4ef7
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-24 16:48:41 -0300

    full broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   after_create_commit { broadcast_append_to "messages", target: "messages" }
+  after_update_commit { broadcast_replace_to "messages", target: "message_#{self.id}"  }
+  after_destroy_commit { broadcast_remove_to "messages", target: "message_#{self.id}"  }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   has_many :messages
   after_create_commit { broadcast_append_to "chat_rooms", target: "chat_rooms" }
+  after_update_commit { broadcast_replace_to "chat_rooms", target: "room_#{self.id}"  }
+  after_destroy_commit { broadcast_remove_to "chat_rooms", target: "room_#{self.id}"  }
 end
