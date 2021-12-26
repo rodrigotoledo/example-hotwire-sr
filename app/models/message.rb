325592a708c81ac6820b14ef1085a383484185de
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-25 23:38:53 -0300

    ui/ux to devise

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,12 +3,12 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   after_create_commit do
-    broadcast_append_later_to "messages_#{self.room_id}", target: "messages_#{self.room_id}", locals: { user: self.user }
-    broadcast_update_later_to "messages", target: "count_message_#{self.room_id}", html: self.room.messages.count, locals: { user: self.user }
+    broadcast_append_to "messages_#{self.room_id}", target: "messages_#{self.room_id}"
+    broadcast_update_to "messages", target: "count_message_#{self.room_id}", html: self.room.messages.count
   end
-  after_update_commit { broadcast_replace_to "message_#{self.id}", target: "message_#{self.id}", locals: { user: self.user }  }
+  after_update_commit { broadcast_replace_to "message_#{self.id}", target: "message_#{self.id}"  }
   after_destroy_commit do
-    broadcast_remove_to "messages", target: "message_#{self.id}", locals: { user: self.user }
-    broadcast_update_to "messages", target: "count_message_#{self.room_id}", html: self.room.messages.count, locals: { user: self.user }
+    broadcast_remove_to "messages", target: "message_#{self.id}"
+    broadcast_update_to "messages", target: "count_message_#{self.room_id}", html: self.room.messages.count
   end
 end
